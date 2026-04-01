ae2f5fe7cc787d239d353a5f1c7d231fbd5e3acf
Sameeksha <bramhapurikarsameeksha@gmail.com>
2026-04-01 20:26:47 +0530

    Create StartNoseOfLoopInLL.java

---
+++ Linked List/StartNoseOfLoopInLL.java
@@ -0,0 +1,43 @@
+/*Definition of singly linked list:
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+        val = 0;
+        next = null;
+    }
+
+    ListNode(int data1) {
+        val = data1;
+        next = null;
+    }
+
+    ListNode(int data1, ListNode next1) {
+        val = data1;
+        next = next1;
+    }
+}
+ */
+
+class Solution {
+    public ListNode findStartingPoint(ListNode head) {
+        ListNode slow=head;
+        ListNode fast=head;
+        while(fast!=null && fast.next!=null)
+        {
+            slow=slow.next;
+            fast=fast.next.next;
+            if(slow==fast){
+                slow=head;
+                while(slow!=fast){
+                    slow=slow.next;
+                    fast=fast.next;
+                }
+                return slow;
+            }
+               
+        }
+    return null;
+    }
+}
