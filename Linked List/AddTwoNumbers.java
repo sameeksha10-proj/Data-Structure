09be1ba9117d444b6a6f282f17245a0e219ed3da
Sameeksha <bramhapurikarsameeksha@gmail.com>
2026-04-09 22:51:21 +0530

    Create AddTwoNumbers.java

---
+++ Linked List/AddTwoNumbers.java
@@ -0,0 +1,35 @@
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode temp1=l1;
+        ListNode temp2=l2;
+        ListNode dummyNode=new ListNode(-1);
+        ListNode curr=dummyNode;
+        int carry=0;
+        while(temp1!=null || temp2!=null)
+        {
+            int sum=carry;
+            if(temp1!=null)
+            {
+                sum=sum+temp1.val;
+                temp1=temp1.next;
+            }
+            if(temp2!=null)
+            {
+                sum=sum+temp2.val;
+                temp2=temp2.next;
+            }
+            ListNode newNode=new ListNode(sum%10);
+            carry=sum/10;
+
+            curr.next=newNode;
+            curr=curr.next;
+        }
+        if(carry!=0)
+        {
+            ListNode newNode= new ListNode(carry);
+            curr.next=newNode;
+        }
+
+    return dummyNode.next;
+    }
+}
