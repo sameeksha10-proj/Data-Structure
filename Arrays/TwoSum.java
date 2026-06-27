0a97a3bf97b46d18e26635018d2542c836ff525e
Sameeksha <bramhapurikarsameeksha@gmail.com>
2026-06-27 18:18:56 +0530

    Add binary search to twoSum for improved performance
    Implemented binary search to optimize twoSum method using sorting.

--- Arrays/TwoSum.java
+++ Arrays/TwoSum.java
@@ -14,3 +14,48 @@ class Solution {
      return new int[]{-1,-1};
     }
 }
+
+
+class Solution {
+    
+    private boolean binarySearch(int[] arr,int left,int right,int target)
+    {
+        int n=arr.length;
+        left=0;
+        right=n-1;
+        while(left < right)
+        {
+            int mid=((left+right)/2);
+            if(arr[mid]==target)
+            {
+                return true;
+            }
+            if(arr[mid] < target)
+            {
+                left=mid+1;
+            }
+            else 
+            {
+                right=mid-1;
+            }
+        }
+           return false;
+    }
+    //using sorting
+    boolean twoSum(int arr[], int target) {
+        Arrays.sort(arr);
+        int n=arr.length;
+        for(int i=0;i<n;i++)
+        {
+            int a=arr[i];
+            int more= target - a;
+            
+            if(binarySearch(arr,i+1,n-1,more))
+            {
+                return true;
+            }
+        }
+        return false;
+        
+    }
+}
